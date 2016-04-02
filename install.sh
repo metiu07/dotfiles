bcf582a556a078fdb89f7dfc98177490d9c3f01e
metiu07 <metiu07@gmail.com>
2016-04-02 19:15:29 +0200

    Simple install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # This script will copy all files to home directory
 # To install Vundle for vim you can use 
-# $ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+cp ./.bash_profile ~
 cp ./.tmux.conf ~
+cp ./.vimrc ~
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
