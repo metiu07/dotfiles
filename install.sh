8bcc85856ce3dc7815984d1cc9fc90850994cf62
metiu07 <metiu07@gmail.com>
2016-04-03 11:04:01 +0200

    Install tmux plugin manager

--- install.sh
+++ install.sh
@@ -5,4 +5,11 @@ cp ./.bash_profile ~
 cp ./.tmux.conf ~
 cp ./.vimrc ~
 
+# Install Vundle for vim plugin management
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# use $ vim +PluginInstall +qall
+# to install all plugins from .vimrc
+
+# Install Tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# use prefix{`} + I for installing plugins from .tmux.conf
