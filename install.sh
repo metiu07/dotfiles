70b7f4ce4bf79246f2439d09645098f37fc909e6
metiu07 <metiu07@gmail.com>
2016-04-08 21:48:51 +0200

    Added backup to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 # This script will copy all files to home directory
 # To install Vundle for vim you can use 
 
+# Backup old files
+mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.tmux.conf ~/.tmux.conf.bak
+mv ~/.vimrc ~/.vimrc.bak
+
 cp ./.bash_profile ~
 cp ./.tmux.conf ~
 cp ./.vimrc ~
@@ -11,5 +16,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # to install all plugins from .vimrc
 
 # Install Tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # use prefix{`} + I for installing plugins from .tmux.conf
