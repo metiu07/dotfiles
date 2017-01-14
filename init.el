eee98cd1790f3c743daa7f626b5b073d307bc04d
metiu07 <metiu07@gmail.com>
2017-01-14 19:05:49 +0100

    Rework of emacs init file

--- init.el
+++ init.el
@@ -1,42 +1,25 @@
+;; Initialize package repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
+;; Initializes packages, so we can use -> use-package
 (package-initialize)
 
-;; if package is not present install it
-(defun require-package (package)
-  (setq-default highlight-tabs t)
-  "Install given PACKAGE"
-  (unless (package-installed-p package)
-    (unless (assoc package package-archive-contents)
-      (package-refresh-contents))
-    (package-install package)))
+;; Ensure that we have every package we need
+(setq use-package-always-ensure t)
 
-;; Electric pair mode
+;; VIM initialization
+(use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode t))
+
+;;; BEHAVIOUR
+;; Write both brackets
 (electric-pair-mode)
-
-;; backup directory
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; save cursor position
-(require-package 'saveplace)
-(setq save-place-file "~/.emacs.d/saveplace")
-(setq-default save-place t)
-
-;; relative line numbers
-;; (require-package 'relative-line-numbers)
-;; (add-hook 'prog-mode-hook 'relative-line-numbers-mode t)
-;; (add-hook 'prog-mode-hook 'line-number-mode t)
-;; (add-hook 'prog-mode-hook 'column-number-mode t)
-
-;; ediff: side-by-side diffs
-(setq ediff-split-window-function 'split-window-horizontally
-      ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;; IDE-like GDB
 (setq gdb-many-windows t)
@@ -44,219 +27,15 @@
 ;; Turn off line truncating
 (set-default 'truncate-lines -1)
 
-;; Smart tabs enable
-;; (smart-tabs-insinuate 'c 'javascript)
+;; Smart indentation enable
 (setq indent-tabs-mode t)
 (setq c-basic-offset 4)
 (setq tab-width 4)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#657b83")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes
-   (quote
-    ("70b51a849b665f50a97a028c44cec36b398398357d8f7c19d558fe832b91980f" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(ede-project-directories (quote ("/Users/mato/dev/osdev")))
- '(fci-rule-color "#eee8d5")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#fdf6e3" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors
-   (quote
-    (("#eee8d5" . 0)
-     ("#B4C342" . 20)
-     ("#69CABF" . 30)
-     ("#69B7F0" . 50)
-     ("#DEB542" . 60)
-     ("#F2804F" . 70)
-     ("#F771AC" . 85)
-     ("#eee8d5" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
- '(hl-fg-colors
-   (quote
-    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(pos-tip-background-color "#eee8d5")
- '(pos-tip-foreground-color "#586e75")
- '(ring-bell-function (quote ignore) t)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
- '(term-default-bg-color "#fdf6e3")
- '(term-default-fg-color "#657b83")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c37300")
-     (60 . "#b97d00")
-     (80 . "#b58900")
-     (100 . "#a18700")
-     (120 . "#9b8700")
-     (140 . "#948700")
-     (160 . "#8d8700")
-     (180 . "#859900")
-     (200 . "#5a942c")
-     (220 . "#439b43")
-     (240 . "#2da159")
-     (260 . "#16a870")
-     (280 . "#2aa198")
-     (300 . "#009fa7")
-     (320 . "#0097b7")
-     (340 . "#008fc7")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
- '(xterm-color-names
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
- '(xterm-color-names-bright
-   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
-
-;; no bell
+;; Never show visual bell
 (setq visible-bell nil)
 
-;; Auto indent lines after pressing return so I don't have to press TAB
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-;; remove whitespace before saving
-;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; Autocomplete
-(require-package 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; flycheck
-(require-package 'flycheck)
-
-;; evil plugins
-(require-package 'evil-surround)
-(require-package 'evil-tabs)
-(require-package 'evil-leader)
-(require-package 'evil-magit)
-
-(setq evil-want-C-u-scroll t)
-
-(require 'evil)
-
-(global-evil-surround-mode t)
-(global-evil-leader-mode t)
-
-(evil-mode t)
-
-;; Cursor color depending on mode
-(setq evil-emacs-state-cursor '("red" box))
-(setq evil-normal-state-cursor '("green" box))
-(setq evil-visual-state-cursor '("orange" box))
-(setq evil-insert-state-cursor '("red" bar))
-(setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
-
-;; GGtags
-;; (require 'ggtags)
-;; (add-hook 'c-mode-common-hook
-;; 	  (lambda()
-;; 	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-;; 	      (ggtags-mode 1))))
-
-;; Projectile
-(require 'projectile)
-(require 'helm-projectile)
-
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-
-;; Helm-gtags
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t)
-
-(require-package 'helm-gtags)
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-;; Helm
-(require 'helm)
-(require-package 'helm-swoop)
-(require 'helm-config)
-
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-;; (define-key helm-map (kbd "C-z") 'helm-select-action)
-
-(helm-mode 1)
-
-;; Org Mode
-(require 'org)
-(setq org-hide-leading-stars t)
-
-(org-babel-do-load-languages 'org-babel-load-languages '((python . t)))
-
-;; GUI
-;; disable menu bar {file, ...}
-(menu-bar-mode -1)
-
-;; KEYBINDS
-;; <CTRL>-hjkl to move between windows
-;; (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-;; (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-
-;; evil leader binds
-;; (evil-leader/set-leader ",")
-(evil-leader/set-key "e" 'helm-find)
-(evil-leader/set-key "x" 'helm-M-x)
-(evil-leader/set-key "b" 'helm-buffers-list)
-(evil-leader/set-key "v" 'helm-bookmarks)
-(evil-leader/set-key "s" 'helm-swoop)
-(evil-leader/set-key "g" 'magit-status)
-(evil-leader/set-key "a" 'org-agenda)
-(evil-leader/set-key "l" 'org-store-link)
-(evil-leader/set-key "i" 'org-iswitchb)
-(evil-leader/set-key "d" 'dired)
-;; Show all tags in current function
-(evil-leader/set-key "a" 'helm-gtags-tags-in-this-function)
-;; Show all possible tags
-(evil-leader/set-key "q" 'helm-gtags-select)
-;; Jump to a tag
-(evil-leader/set-key "t" 'helm-gtags-dwim)
-;; Jump back
-(evil-leader/set-key "o" 'helm-gtags-pop-stack)
-;; Projectile find file
-(evil-leader/set-key "f" 'helm-projectile-find-file)
-
-;; Make helm defaul M-x
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-;;; esc quits
+;; Esc quits interactive lines
 (defun minibuffer-keyboard-quit ()
   "Abort recursive edit.
 In Delete Selection mode, if the mark is active, just deactivate it;
@@ -274,9 +53,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; USER INTERFACE
+;; Disable menu bar
+(menu-bar-mode -1)
