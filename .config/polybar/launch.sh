d0c983ed7ea919b73166d9dbf6873b4a5cffb8c0
metiu07 <metiu07@gmail.com>
2019-02-13 13:25:20 +0100

    polybar: launch script for the multiple monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,51 +5,56 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-desktop=$(echo $DESKTOP_SESSION)
-
-# TODO: Remove other wm
-case $desktop in
-    i3)
-    if type "xrandr" > /dev/null; then
-      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	if [ $m == 'DP2' ]
-	then		
+if type "xrandr" > /dev/null; then
+    monitors_num=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+    if [ $monitors_num -eq 1 ]
+    then
+	echo "Only one monitor"
+	m=$(xrandr --query | grep " connected" | cut -d" " -f1)
+	xrandr --output $m
+	MONITOR=$m polybar --reload main -c ~/.config/polybar/config &
+    elif [ $monitors_num -eq 2 ]
+    then
+	echo "Two monitors"
+	~/.config/polybar/home.sh
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	    if [ $m == "DP2" ]
+	    then
+		echo "Main monitor"
 		MONITOR=$m polybar --reload main -c ~/.config/polybar/config &	
-	elif [ $m == 'eDP1' ]
-	then
+	    else
 		MONITOR=$m polybar --reload secondary -c ~/.config/polybar/config &
-	else
+	    fi
+	done
+    elif [ $monitors_num -eq 3 ]
+    then
+	echo "Three monitors"
+	~/.config/polybar/work.sh
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	    if [ $m == "DP1-1" ]
+	    then
+		echo "Main monitor"
+		MONITOR=$m polybar --reload main -c ~/.config/polybar/config &
+	    else
 		MONITOR=$m polybar --reload secondary -c ~/.config/polybar/config &
-	fi     
-      done
+	    fi
+	done
     else
+	echo "Multiple monitors"
+	xrandr --output eDP1 --right-of DP2
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	    echo "Monitor: $m"
+	    if [ $m == 'DP2' ] || [ $m == 'DP1-1']
+	    then
+		MONITOR=$m polybar --reload main -c ~/.config/polybar/config &	
+	    elif [ $m == 'eDP1' ]
+	    then
+		MONITOR=$m polybar --reload secondary -c ~/.config/polybar/config &
+	    else
+		MONITOR=$m polybar --reload secondary -c ~/.config/polybar/config &
+	    fi
+	done
+    fi
+else
     polybar --reload main -c ~/.config/polybar/config &
-    fi
-    ;;
-    openbox)
-    if type "xrandr" > /dev/null; then
-      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar-openbox -c ~/.config/polybar/config &
-      done
-    else
-    polybar --reload mainbar-openbox -c ~/.config/polybar/config &
-    fi
-#    if type "xrandr" > /dev/null; then
-#      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#        MONITOR=$m polybar --reload mainbar-openbox-extra -c ~/.config/polybar/config &
-#      done
-#    else
-#    polybar --reload mainbar-openbox-extra -c ~/.config/polybar/config &
-#    fi
-
-    ;;
-    bspwm)
-    if type "xrandr" > /dev/null; then
-      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
-      done
-    else
-    polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
-    fi
-    ;;
-esac
+fi
