036859a248f6e21ba04c6eb611021be8008fa2cb
metiu07 <metiu07@gmail.com>
2019-06-23 09:36:56 +0200

    Emacs: doom config

---
+++ .config/doom/config.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+(after! org (set-face-attribute 'org-level-1 nil :height 1.7)
+  (set-face-attribute 'org-level-2 nil :height 1.5)
+  (set-face-attribute 'org-level-3 nil :height 1.3))
