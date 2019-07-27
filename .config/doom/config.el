f7e12dc4228ca35769968c8a1acc6d7cd64b5cac
metiu07 <metiu07@gmail.com>
2019-07-27 19:11:33 +0200

    Emacs: set face for org-level-1 to be transparent

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,5 +1,5 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(after! org (set-face-attribute 'org-level-1 nil :height 1.7)
+(after! org (set-face-attribute 'org-level-1 nil :background nil :foreground "color-26" :height 1.7)
   (set-face-attribute 'org-level-2 nil :height 1.5)
   (set-face-attribute 'org-level-3 nil :height 1.3))
