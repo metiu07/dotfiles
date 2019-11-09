d13dbdfa4feb0b3094ba62dd4ea759c231536998
metiu07 <metiu07@gmail.com>
2019-11-09 13:32:22 +0100

    fish: white_bg and black_bg background abreviation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,7 @@ set -gx VIRTUAL_ENV_DISABLE_PROMPT YES
 
 set -gx MAKEFLAGS '-j'(nproc)
 
-bind \cr history-search-backward
+# Dynamicaly set the background color
+# TODO: Move to aliases?
+abbr -a -g white_bg 'printf "\033Ptmux;\033\033]11;white\007\033\\\\"'
+abbr -a -g black_bg 'printf "\033Ptmux;\033\033]11;black\007\033\\\\"'
