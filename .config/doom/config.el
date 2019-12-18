c36022ff6689f4d59d07dbc9ca45232340725e7e
metiu07 <metiu07@gmail.com>
2019-12-18 01:49:55 +0100

    Emacs: Make :W behave the same as :w

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -3,3 +3,6 @@
 (after! org (set-face-attribute 'org-level-1 nil :background nil :foreground "color-26" :height 1.7)
   (set-face-attribute 'org-level-2 nil :height 1.5)
   (set-face-attribute 'org-level-3 nil :height 1.3))
+
+(after! evil
+  (evil-ex-define-cmd "W" "write"))
