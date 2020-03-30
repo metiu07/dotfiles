5916cbc27e293c8c9c710a20e40b282165933afe
metiu07 <metiu07@gmail.com>
2020-03-30 17:38:32 +0200

    fish: Also install plugins

---
+++ scripts/install_fish.fish
@@ -0,0 +1,6 @@
+#!/bin/fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+fisher add jethrokuan/z
+fisher add rafaelrinaldi/pure
+fisher add laughedelic/pisces
--- scripts/install_fish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/fish
-
-curl -L https://get.oh-my.fish
-
-omf install z
-omf install expand
