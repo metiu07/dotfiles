35d621846b8e882882216d860a74886cb574f968
metiu07 <metiu07@gmail.com>
2021-04-04 01:35:14 +0200

    scripts: Vim install check if the vim is installed

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -2,5 +2,10 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/vim/bundle/Vundle.vim
 
-vim +PluginInstall +qall || true
-nvim +PluginInstall +qall
+if ! command -v vim &> /dev/null; then
+    vim +PluginInstall +qall
+fi
+
+if ! command -v nvim &> /dev/null; then
+    nvim +PluginInstall +qall
+fi
