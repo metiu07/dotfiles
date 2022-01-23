97294a7f0e640627d7a9d0ae2702b9c9e704633a
metiu07 <metiu07@gmail.com>
2022-01-23 14:31:18 +0100

    fish: Add plugins

--- scripts/install_fish.fish
+++ scripts/install_fish.fish
@@ -2,5 +2,7 @@
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 fisher install jethrokuan/z
-fisher install rafaelrinaldi/pure
+# fisher install rafaelrinaldi/pure
+fisher install MatejKastak/pure@kube_ps1
+fisher install MatejKastak/fishmak
 fisher install laughedelic/pisces
