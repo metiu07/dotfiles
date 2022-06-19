f1acbf35f021c5d64374472a79d8afccd3932354
metiu07 <metiu07@gmail.com>
2022-06-19 13:23:24 +0200

    chore(install.py): Reformat using black

--- install.py
+++ install.py
@@ -15,8 +15,6 @@ import subprocess
 import sys
 from typing import Dict, List, Optional
 
-
-
 # TODO: Add Ctrl-C handler
 # TODO: Rename to dotfiles
 # TODO: Create symlink to ~/.local/bin/dotfiles
@@ -34,9 +32,9 @@ class Installer:
         self.vprint('Parsed args "{}"'.format(args))
 
         self.install_mode = False if args.remove else True
-        self.backup_ext = '.bak'
-        self.install_cmd = ['mv'] if args.move else ['ln', '-sf']
-        self.remove_cmd = ['rm']
+        self.backup_ext = ".bak"
+        self.install_cmd = ["mv"] if args.move else ["ln", "-sf"]
+        self.remove_cmd = ["rm"]
         self.auto_confirm = args.auto_confirm
 
         # Parse the json
@@ -58,15 +56,17 @@ class Installer:
 
         # Ask user to uninstall modules
         if not self.install_mode:
-            print('Would remove "{}" modules.'.format(' '.join(args.modules)))
-            if not self.confirm('Do you want to proceed?', default=False, auto_confirm=True):
-                sys.exit('Aborting... Don\'t uninstall modules.')
+            print('Would remove "{}" modules.'.format(" ".join(args.modules)))
+            if not self.confirm(
+                "Do you want to proceed?", default=False, auto_confirm=True
+            ):
+                sys.exit("Aborting... Don't uninstall modules.")
 
         # Install configurations
         self.run(args.modules)
 
         if not self.dry_run and self.processed_modules:
-            print('Enjoy your new configuration :)')
+            print("Enjoy your new configuration :)")
 
     def load_configuration(self, input_file: str) -> Dict[str, str]:
         """Load the json configuration.
@@ -79,7 +79,9 @@ class Installer:
                 raw_json_data = f.read()
             return json.loads(raw_json_data)
         except json.decoder.JSONDecodeError as err:
-            sys.exit('Input file "{}" can not be parsed.\n{}'.format(input_file, str(err)))
+            sys.exit(
+                'Input file "{}" can not be parsed.\n{}'.format(input_file, str(err))
+            )
         except FileNotFoundError:
             sys.exit('Configuration file "{}" not found.'.format(input_file))
 
@@ -88,7 +90,7 @@ class Installer:
 
         for m in modules:
 
-            self.vprint('Processing module: {}'.format(m))
+            self.vprint("Processing module: {}".format(m))
 
             if m not in self.confs:
                 sys.exit('Aborting... can not find the configuration "{}"'.format(m))
@@ -102,43 +104,60 @@ class Installer:
                 self.vprint('Skipping... already processed module "{}"'.format(m))
                 continue
 
-            if {'source', 'destination'} <= set(c):
+            if {"source", "destination"} <= set(c):
                 self.backup(c)
                 self.action(c)
                 self.processed_modules.append(m)
 
-            if '$deps' in c:
-                self.vprint('Resolving dependencies')
+            if "$deps" in c:
+                self.vprint("Resolving dependencies")
                 self.processed_modules.append(m)
-                self.run(c['$deps'])
+                self.run(c["$deps"])
 
-            if self.install_mode and 'install' in c:
-                install_script = c['install']
+            if self.install_mode and "install" in c:
+                install_script = c["install"]
                 if os.path.exists(install_script):
-                    print('Target "{}" wants to run script "{}"'.format(m, install_script))
-                    if self.confirm('Do you want to view the file?', default=False):
-                        self.exec_command(['less', install_script])
+                    print(
+                        'Target "{}" wants to run script "{}"'.format(m, install_script)
+                    )
+                    if self.confirm("Do you want to view the file?", default=False):
+                        self.exec_command(["less", install_script])
                     if not self.confirm('Install "{}"?'.format(m)):
-                        sys.exit('Aborting... Install script for "{}" @ "{}" was not allowed to run.'.format(m, c['install']))
-                    self.exec_command([c['install']])
+                        sys.exit(
+                            'Aborting... Install script for "{}" @ "{}" was not allowed to run.'.format(
+                                m, c["install"]
+                            )
+                        )
+                    self.exec_command([c["install"]])
                 else:
-                    sys.exit('Aborting... Install script for "{}" @ "{}" does not exist.'.format(m, c['install']))
-
+                    sys.exit(
+                        'Aborting... Install script for "{}" @ "{}" does not exist.'.format(
+                            m, c["install"]
+                        )
+                    )
 
     def backup(self, module):
         """If file is present and not symlink, create its backup."""
 
-        dest = self.prepare_path(module['destination'])
+        dest = self.prepare_path(module["destination"])
         if os.path.exists(dest) and not os.path.islink(dest):
             # Copy the file with backup extension
-            self.exec_command(['mv', dest, dest + self.backup_ext])
+            self.exec_command(["mv", dest, dest + self.backup_ext])
 
     def install(self, module):
         """Install the file with specified command."""
 
         # Create directories if they don't exists already
-        os.makedirs(os.path.dirname(self.prepare_path(module['destination'])), exist_ok=True)
-        self.exec_command(self.install_cmd + [self.prepare_path(module['source']), self.prepare_path(module['destination'])])
+        os.makedirs(
+            os.path.dirname(self.prepare_path(module["destination"])), exist_ok=True
+        )
+        self.exec_command(
+            self.install_cmd
+            + [
+                self.prepare_path(module["source"]),
+                self.prepare_path(module["destination"]),
+            ]
+        )
 
     def remove_conf(self, module):
         """Remove the configuration file.
@@ -146,15 +165,17 @@ class Installer:
         Warning: This may permanently destroy your configuration.
         """
 
-        self.exec_command(self.remove_cmd + [module['destination']])
+        self.exec_command(self.remove_cmd + [module["destination"]])
 
-    def exec_command(self, c: List[str], check=True, **kwargs) -> subprocess.CompletedProcess:
+    def exec_command(
+        self, c: List[str], check=True, **kwargs
+    ) -> subprocess.CompletedProcess:
         """Execute the commnad, in case of dry run just print it to user."""
 
         if self.dry_run:
-            print(' '.join(c))
+            print(" ".join(c))
             # Maybe there is a better way to return empty program
-            return subprocess.run(['true'])
+            return subprocess.run(["true"])
         else:
             try:
                 return subprocess.run(c, check=check, **kwargs)
@@ -172,47 +193,70 @@ class Installer:
         if self.verbose:
             print(s)
 
-    def confirm(self, s: str, default: bool=True, auto_confirm: Optional[bool]=None) -> bool:
+    def confirm(
+        self, s: str, default: bool = True, auto_confirm: Optional[bool] = None
+    ) -> bool:
         """Ask user for confirmation."""
 
         if self.auto_confirm:
             return auto_confirm or default
 
-        prompt = '{} {} '.format(s, '[Y/n]' if default else '[y/N]')
+        prompt = "{} {} ".format(s, "[Y/n]" if default else "[y/N]")
         while True:
             ans = input(prompt)
-            if ans == '':
+            if ans == "":
                 return default
-            elif ans.lower() in ['y']:
+            elif ans.lower() in ["y"]:
                 return True
-            elif ans.lower() in ['n']:
+            elif ans.lower() in ["n"]:
                 return False
 
-            print('Please answer [y, n]')
+            print("Please answer [y, n]")
 
 
 def parse_arguments():
     """Setup and parse the program arguments."""
 
     parser = argparse.ArgumentParser(
-        description='Dotfiles installer program to symlink config files for user') # noqa
-    parser.add_argument('modules', nargs='*', default=['linux'])
-    parser.add_argument('-r', '--remove', action='store_true',
-                        help='uninstall selected modules')
-    parser.add_argument('-d', '--dry', action='store_true',
-                        help='dry run, only print commnads don\'t execute')
-    parser.add_argument('-m', '--move', action='store_true',
-                        help='install by moving instead of creating symlinks')
-    parser.add_argument('-l', '--list-modules', action='store_true',
-                        help='list all modules in INPUT file')
-    parser.add_argument('-a', '--auto-confirm', action='store_true',
-                        help='skip all user confirmations')
-    parser.add_argument('-i', '--input', action='store',
-                        default='./configuration.json',
-                        help='set the input file (default: ./configuration.json)') # noqa
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose output') # noqa
+        description="Dotfiles installer program to symlink config files for user"
+    )  # noqa
+    parser.add_argument("modules", nargs="*", default=["linux"])
+    parser.add_argument(
+        "-r", "--remove", action="store_true", help="uninstall selected modules"
+    )
+    parser.add_argument(
+        "-d",
+        "--dry",
+        action="store_true",
+        help="dry run, only print commnads don't execute",
+    )
+    parser.add_argument(
+        "-m",
+        "--move",
+        action="store_true",
+        help="install by moving instead of creating symlinks",
+    )
+    parser.add_argument(
+        "-l",
+        "--list-modules",
+        action="store_true",
+        help="list all modules in INPUT file",
+    )
+    parser.add_argument(
+        "-a", "--auto-confirm", action="store_true", help="skip all user confirmations"
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        action="store",
+        default="./configuration.json",
+        help="set the input file (default: ./configuration.json)",
+    )  # noqa
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="verbose output"
+    )  # noqa
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Installer(parse_arguments())
