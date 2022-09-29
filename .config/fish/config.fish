0bccb960b191b986690781955459294fdca0f0cf
metiu07 <metiu07@gmail.com>
2022-09-29 11:38:49 +0200

    feat(fish): Remove unused qwerty bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,27 +8,6 @@
 # Initialize aliases
 source ~/.config/fish/aliases
 
-function fish_qwerty_key_bindings -d "Setup QWERTY bindings"
-    # fish_vi_key_bindings
-
-    bind \cO -M insert 'cd -; commandline -f repaint'
-    bind \cO 'cd -; commandline -f repaint'
-    bind \cF -M insert accept-autosuggestion
-    bind \cF accept-autosuggestion
-    bind \cP -M insert up-or-search
-    bind \cP up-or-search
-    bind \cN -M insert down-or-search
-    bind \cE -M insert end-of-line
-    bind \cE end-of-line
-    bind \cA -M insert beginning-of-line
-    bind \cA beginning-of-line
-
-    bind \cR _fzf-multi-command-history-widget
-    bind \cR -M insert _fzf-multi-command-history-widget
-
-    fzf_key_bindings
-end
-
 # Vim
 # `fish_key_reader` - check the SEQUENCE
 # `bind -f`         - show possible commands
