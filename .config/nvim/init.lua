a2a5124fa07f930fa9ef4b6206e4f42e99817733
metiu07 <metiu07@gmail.com>
2022-12-11 14:06:26 +0100

    feat(vim): Telescope find_files also show hidden

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,6 +86,14 @@ require('packer').startup(function(use)
 
 end)
 
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            hidden = true
+        },
+    },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
