560531e8ec1038afef7db0e4fa791d4422015445
metiu07 <metiu07@gmail.com>
2023-03-17 18:37:48 +0100

    feat(vim): Add diffview plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,6 +60,7 @@ require('packer').startup(function(use)
     use 'airblade/vim-rooter'
     use 'tpope/vim-fugitive'
     use 'lewis6991/gitsigns.nvim'
+    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
     -- LSP
     use 'neovim/nvim-lspconfig'
