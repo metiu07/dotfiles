44f51cad16ce54af1adf9b139c095a7d5a08d5b4
metiu07 <metiu07@gmail.com>
2023-03-22 10:01:50 +0100

    feat(vim): Fix the persistent lightbulb in lspsaga

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,7 +68,15 @@ require('packer').startup(function(use)
         "glepnir/lspsaga.nvim",
         branch = "main",
         config = function()
-            require("lspsaga").setup({})
+            require('lspsaga').setup({
+                lightbulb = {
+                    enable = false,
+                    enable_in_insert = false,
+                    sign = false,
+                    sign_priority = 40,
+                    virtual_text = false,
+                },
+            })
         end,
         requires = {
             { "nvim-tree/nvim-web-devicons" },
