261219732d3c9588454b05543f91708a0489f27d
metiu07 <metiu07@gmail.com>
2023-08-25 14:39:26 +0200

    feat(vim): Remove unused nvim-cmp mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -289,8 +289,6 @@ cmp.setup({
                 fallback()
             end
         end, { "i", "s" }),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
