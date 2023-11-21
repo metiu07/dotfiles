d509de4a503550b49b90415dc38bd983c7aa6f38
metiu07 <metiu07@gmail.com>
2023-11-21 20:31:12 +0100

    feat(fish): Enable light fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,11 +47,15 @@ else
 end
 
 # Set the fzf color theme (https://github.com/junegunn/fzf/wiki/Color-schemes)
-# Gruvbox
+# Light
 set -x FZF_DEFAULT_OPTS "\
-   --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
-   --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
+   --color light
    --bind=\"ctrl-k:down,ctrl-e:up\""
+# Gruvbox
+# set -x FZF_DEFAULT_OPTS "\
+#    --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
+#    --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
+#    --bind=\"ctrl-k:down,ctrl-e:up\""
 # Nord
 # set -x FZF_DEFAULT_OPTS "\
 #    --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C \
