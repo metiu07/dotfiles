5166e627a78b39f2e79cc1287799a9f5f415982b
metiu07 <metiu07@gmail.com>
2024-01-07 10:09:23 +0100

    feat(nixos): Add new host

---
+++ nixos/mage/configuration.nix
@@ -0,0 +1,186 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  # Set the kernel
+  boot.kernelPackages = pkgs.linuxPackages_hardened;
+
+  networking.hostName = "rpi";
+  networking.networkmanager.enable = true;
+
+  # Firewall
+  networking.firewall.enable = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Bratislava";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  programs.fish.enable = true;
+
+  users.users = {
+    anon = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [ ];
+      shell = pkgs.fish;
+    };
+  };
+
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 4*1024;
+  } ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "no";
+  services.openssh.settings.PasswordAuthentication = false;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    btop
+    curl
+    delta
+    docker-compose
+    entr
+    eza
+    fd
+    ffmpeg
+    file
+    fishPlugins.fzf-fish
+    fwupd
+    fzf
+    gcc
+    gdb
+    git
+    glib
+    gnumake
+    htop
+    imagemagick
+    imhex
+    jq
+    libqalculate
+    linuxHeaders
+    lshw
+    lsof
+    ltrace
+    ncdu
+    neovim
+    netcat-gnu
+    openssh
+    poetry
+    powertop
+    psmisc
+    pyright
+    python311Full
+    qrencode
+    ranger
+    ripgrep
+    rustup
+    starship
+    strace
+    tcpdump
+    tmux
+    torsocks
+    traceroute
+    tree
+    unixtools.fdisk
+    unixtools.xxd
+    unzip
+    upower
+    util-linux
+    wget
+    xdg-utils
+    yq-go
+    zip
+    zoxide
+    zstd 
+  ];
+}
+
