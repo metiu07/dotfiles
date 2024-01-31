cf8e66ab345b17ae5e9aecbe7019d8c3326a1bb1
metiu07 <metiu07@gmail.com>
2024-01-31 17:43:56 +0100

    feat(vim): Auto install bash tree sitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -507,8 +507,18 @@ require('lazy').setup({
         build = ":TSUpdate",
         config = function()
             require('nvim-treesitter.configs').setup({
-                ensure_installed = { "c", "diff", "lua", "rust", "fish", "markdown", "markdown_inline", "javascript",
-                    "typescript" },
+                ensure_installed = {
+                    "c",
+                    "bash",
+                    "diff",
+                    "lua",
+                    "rust",
+                    "fish",
+                    "markdown",
+                    "markdown_inline",
+                    "javascript",
+                    "typescript",
+                },
                 sync_install = false,
                 auto_install = true,
                 highlight = {
