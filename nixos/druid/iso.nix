b1dbc1ca6928321d726ca521ae2879fadce5be89
metiu07 <metiu07@gmail.com>
2024-02-19 19:47:50 +0100

    feat(nix): Update druid ISO

--- nixos/druid/iso.nix
+++ nixos/druid/iso.nix
@@ -1,10 +1,16 @@
 # Building:
+# ```bash
+# # I had some issues with this command
 # nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
+# 
+# git clone -b nixos-23.11 https://github.com/NixOS/nixpkgs --depth 1 /tmp/nixpkgs
+# nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixpkgs=/tmp/nixpkgs/ -I nixos-config=iso.nix
+# ```
 
 { config, pkgs, ... }:
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix>
 
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
@@ -30,13 +36,21 @@
     "rtl88xxau-aircrack"
   ];
 
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us,sk";
+    xkbVariant = "colemak_dh,qwerty";
+    xkbOptions = "caps:escape,altwin:swap_lalt_lwin";
+  };
 
   environment.systemPackages = with pkgs; [
     alacritty
     btop
     btrfs-progs
     curl
+    dosfstools
     exfatprogs
+    fatresize
     fd
     file
     fish
