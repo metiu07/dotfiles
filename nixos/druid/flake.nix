860141cbc2a48058d52dfc47507fd7898471ee6d
metiu07 <metiu07@gmail.com>
2024-05-29 19:38:26 +0200

    feat(nix): Start using flakes

---
+++ nixos/druid/flake.nix
@@ -0,0 +1,32 @@
+# Based on: https://drakerossman.com/blog/how-to-convert-default-nixos-to-nixos-with-flakes
+# This can be built with nixos-rebuild --flake .#druid build
+{
+  description = "the simplest flake for nixos-rebuild";
+
+  inputs = {
+    nixpkgs = {
+      # url = "github:NixOS/nixpkgs/nixos-23.11";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+      # url = "github:NixOS/nixpkgs/dd37924974b9202f8226ed5d74a252a9785aedf8";
+    };
+  };
+
+  # Outputs can be anything, but the wiki + some commands define their own
+  # specific keys. Wiki page: https://nixos.wiki/wiki/Flakes#Output_schema
+  outputs = { self, nixpkgs }: {
+    # nixosConfigurations is the key that nixos-rebuild looks for.
+    nixosConfigurations = {
+      druid = nixpkgs.lib.nixosSystem {
+        # A lot of times online you will see the use of flake-utils + a
+        # function which iterates over many possible systems. My system
+        # is x86_64-linux, so I'm only going to define that
+        system = "x86_64-linux";
+        # Import our old system configuration.nix
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
+
