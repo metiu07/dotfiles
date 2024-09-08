2a6484906b28584ebc07d4e4cc612fe0b946b6ee
metiu07 <metiu07@gmail.com>
2024-09-08 15:34:36 +0200

    feat(script_get_current_task): improve regex to match task

--- .local/bin/script_get_current_task.sh
+++ .local/bin/script_get_current_task.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fd -e md . $HOME/Documents/notes/ --changed-within 1day -x grep -m 1 '\[~\]' | head -1 | sed 's/^\s*- \[.\] //'
+fd -e md . $HOME/Documents/notes/ --changed-within 1day -x grep -m 1 '\- \[~\]' | head -1 | sed 's/^\s*- \[.\] //'
