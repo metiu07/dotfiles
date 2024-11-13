64cecac74728bbe102eb18464577e83b7d2b0fa0
metiu07 <metiu07@gmail.com>
2024-11-13 12:45:42 +0100

    feat(wezterm): initial configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.keys = {
+    { key = 'UpArrow',   mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
+    { key = 'DownArrow', mods = 'SHIFT', action = act.ActivateCommandPalette },
+}
+config.color_scheme = 'Solarized (light) (terminal.sexy)'
+-- config.color_scheme = 'Solarized Light (Gogh)'
+
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium", bold = false })
+config.font_size = 16.0
+config.cell_width = 0.9
+
+config.enable_tab_bar = false
+
+config.freetype_load_target = "HorizontalLcd"
+
+config.default_cursor_style = "SteadyUnderline"
+
+config.window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+}
+
+config.enable_scroll_bar = false
+
+return config
