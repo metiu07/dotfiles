2258236bf63166d1db4c57c42062504c25c4d268
metiu07 <metiu07@gmail.com>
2025-01-11 21:50:15 +0100

    feat(nvim): Add copilotChat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -553,6 +553,17 @@ require('lazy').setup({
         end,
     },
 
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken",
+        opts = {
+        },
+    },
+
 
     -- Syntax
     {
