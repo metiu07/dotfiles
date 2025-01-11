b76197f7ee962530cecee02e74dcbba24f323e79
metiu07 <metiu07@gmail.com>
2025-01-11 21:50:54 +0100

    feat(nvim): Add orgmode plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -618,6 +618,19 @@ require('lazy').setup({
         end,
     },
 
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = '~/orgfiles/**/*',
+                org_default_notes_file = '~/orgfiles/refile.org',
+            })
+        end,
+    },
+
     -- Color themes
     {
         'morhetz/gruvbox',
