5f13785583626fbd2182e12e4f75e8f0422a8f5c
metiu07 <metiu07@gmail.com>
2025-01-27 15:06:20 +0100

    feat(nvim): Disable copilot for org-mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -546,6 +546,7 @@ require('lazy').setup({
                       \ 'toml': v:true,
                       \ 'json': v:true,
                       \ 'markdown': v:false,
+                      \ 'org': v:false,
                       \ }
                 let g:copilot_no_tab_map = v:true
                 imap <silent><script><expr> <C-F> copilot#Accept("")
